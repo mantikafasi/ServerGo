0e9d854495b2864f87e854cbe1d256961de24da5
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-07-06 15:13:52 +0300

    add constantans

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module server-go
 go 1.16
 
 require (
+	github.com/ravener/discord-oauth2 v0.0.0-20220615092331-f6a9839c223e // indirect
 	github.com/uptrace/bun v1.1.5
 	github.com/uptrace/bun/dialect/pgdialect v1.1.5
 	github.com/uptrace/bun/driver/pgdriver v1.1.5
+	golang.org/x/oauth2 v0.0.0-20220630143837-2104d58473e0 // indirect
 )
--- modules/DiscordUtils.go
+++ modules/DiscordUtils.go
@@ -1,1 +1,23 @@
 package modules
+
+import (
+	"github.com/ravener/discord-oauth2"
+	"golang.org/x/oauth2"
+	"server-go/constantants"
+)
+
+
+func ExchangeCodePlus(token string,redirtect_uri string){
+	conf := &oauth2.Config{
+		Endpoint: discord.Endpoint,
+		Scopes: []string{discord.ScopeIdentify},
+		RedirectURL: redirtect_uri,
+		ClientID: "id",
+		ClientSecret: "secret",
+	}
+	
+}
+
+func ExchangeCode(token string){
+	ExchangeCodePlus(token,constantants.REDIRECT_URI)
+}
