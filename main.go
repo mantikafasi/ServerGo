1891c19f1145a6557522694b235ccbe63d713d37
Manti <67705577+mantikafasi@users.noreply.github.com>
2023-04-24 01:40:36 +0300

    my hate on cors is immeasureable and my life is ruined

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func cors(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
