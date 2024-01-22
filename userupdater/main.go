5a94e6db1af2a22a65dfe944c36bbe65062c49d7
Manti <67705577+mantikafasi@users.noreply.github.com>
2024-01-23 00:06:03 +0300

    update userupdater

--- userupdater/main.go
+++ userupdater/main.go
@@ -8,6 +8,7 @@ import (
 	"server-go/database/schemas"
 	discord_utlils "server-go/modules/discord"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/diamondburned/arikawa/discord"
@@ -15,66 +16,97 @@ import (
 )
 
 func main() {
+	threadCount := 50
 	database.InitDB()
 
-	ArikawaState,err := state.New("Bot " + common.Config.BotToken)
-	
+	ArikawaState, err := state.New("Bot " + common.Config.BotToken)
+
 	if err != nil {
 		panic(err)
 	}
 
 	var allUsers []schemas.URUser
+	botUsers := []schemas.URUser{}
 
 	// get all users that dont have Deleted User in their username
-	err = database.DB.NewSelect().Model(&schemas.URUser{}).Where("username NOT like 'Deleted User%'").Order("id desc").Scan(context.Background(),&allUsers)
+	err = database.DB.NewSelect().Model(&schemas.URUser{}).Where("username NOT like 'Deleted User%'").Order("id desc").Scan(context.Background(), &allUsers)
 	if err != nil {
 		panic(err)
 	}
+	var lock sync.Mutex
 
-	for _, user := range allUsers {
-		// DO NOT UPDATE WARWNING
-		if user.DiscordID == "1134864775000629298" {
-			continue
-		}
+	wg := sync.WaitGroup{}
+	wg.Add(threadCount)
 
-		if user.RefreshToken != "" {
-			if user.AccessTokenExpiry.Before(time.Now()) {
-				token,err := discord_utlils.RefreshToken(user.RefreshToken)
-				if err != nil {
+	for i:=0; i < threadCount; i++ { 
+		go func() {
+			for true {
+				lock.Lock()
+				if len(allUsers) == 0 {
+					wg.Done()
+					lock.Unlock()
+					break;
+				}
+				user := allUsers[len(allUsers) - 1]
 
-					if err.Error() == "invalid_grant" {
-						// refresh token expired, delete it
-						user.RefreshToken = ""
-						user.AccessToken = ""
-						user.AccessTokenExpiry = time.Time{}
-						database.DB.NewUpdate().Model(&user).Where("id = ?", user.ID).OmitZero().Exec(context.Background())
-						continue
-					}
+				allUsers = allUsers[:len(allUsers) - 1]
+				if user.RefreshToken == "" {
+					botUsers = append(botUsers, user)
+					
+					lock.Unlock()
+					continue;
+				}
+				lock.Unlock()
 
-					fmt.Println(err)
+				// DO NOT UPDATE WARWNING
+				if user.DiscordID == "1134864775000629298" {
 					continue
 				}
-
-				user.AccessToken = token.AccessToken
-				user.RefreshToken = token.RefreshToken
-				user.AccessTokenExpiry = token.Expiry
+	
+				if user.AccessTokenExpiry.Before(time.Now()) {
+					token, err := discord_utlils.RefreshToken(user.RefreshToken)
+					if err != nil {
+	
+						if err.Error() == "invalid_grant" {
+							// refresh token expired, delete it
+							user.RefreshToken = ""
+							user.AccessToken = ""
+							user.AccessTokenExpiry = time.Time{}
+							database.DB.NewUpdate().Model(&user).Where("id = ?", user.ID).Exec(context.Background())
+							continue
+						}
+	
+						fmt.Println(err, " ", user.Username, " ", user.DiscordID)
+						continue
+					}
+	
+					user.AccessToken = token.AccessToken
+					user.RefreshToken = token.RefreshToken
+					user.AccessTokenExpiry = token.Expiry
+				}
+	
+				discordUser, err := discord_utlils.GetUser(user.AccessToken)
+				if err == nil {
+					user.Username = common.Ternary(discordUser.Discriminator == "0", discordUser.Username, discordUser.Username+"#"+discordUser.Discriminator)
+					user.AvatarURL = discordUser.AvatarURL()
+				} else {
+					fmt.Println(err)
+				}
+	
+				database.DB.NewUpdate().Model(&user).Where("id = ?", user.ID).OmitZero().Exec(context.Background())
+	
+				println("Updated user via oauth: ", discordUser.Username, " ", user.DiscordID)
 			}
+		}()
+	}
 
-			discordUser,err := discord_utlils.GetUser(user.AccessToken)
-			if err == nil {
-				// update user
-				user.Username = common.Ternary(discordUser.Discriminator == "0", discordUser.Username, discordUser.Username+"#"+discordUser.Discriminator)
-				user.AvatarURL = discordUser.AvatarURL()
-			} else {
-				fmt.Println(err)
-			}
+	wg.Wait()
 
-			database.DB.NewUpdate().Model(&user).Where("id = ?", user.ID).OmitZero().Exec(context.Background())
-			
-			println("Updated user via oauth: ", discordUser.Username, " ", user.DiscordID)
-		} else {
-			
-			discordID,err := strconv.ParseInt(user.DiscordID,10,64)
+	for _, user := range botUsers {
+
+		{
+
+			discordID, err := strconv.ParseInt(user.DiscordID, 10, 64)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -92,7 +124,6 @@ func main() {
 
 			database.DB.NewUpdate().Model(&user).Where("id = ?", user.ID).OmitZero().Exec(context.Background())
 			println("Updated user via bot ", discordUser.Username, " ", user.DiscordID)
-
 		}
 	}
 }
