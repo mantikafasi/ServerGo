5213cab13b3fee0db772f9bd368c1f86b4cc7f24
Manti <67705577+mantikafasi@users.noreply.github.com>
2024-03-31 23:23:31 +0300

    try to fix issues

--- modules/good_person.go
+++ modules/good_person.go
@@ -20,7 +20,7 @@ out:
 
 			filtered := strings.Replace(lower, " "+badNoun, " "+common.GoodPersonConfig.ReplacementNouns[ix], -1)
 
-			if strings.TrimSpace(filtered) != word {
+			if strings.TrimSpace(filtered) != lower {
 				newText += strings.TrimSpace(filtered) + " "
 				continue out
 			}
@@ -31,7 +31,7 @@ out:
 
 			filtered := strings.Replace(lower, " "+badVerb, " "+common.GoodPersonConfig.ReplacementVerbs[ix], -1)
 
-			if strings.TrimSpace(filtered) != word {
+			if strings.TrimSpace(filtered) != lower {
 				newText += strings.TrimSpace(filtered) + " "
 				continue out
 			}
@@ -39,5 +39,5 @@ out:
 
 		newText += word + " "
 	}
-	return newText
+	return strings.TrimSpace(newText)
 }
