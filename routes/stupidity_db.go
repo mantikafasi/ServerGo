95644ae8f784c6b0c91c15e94fdf5b0168dd3d7c
Manti <67705577+mantikafasi@users.noreply.github.com>
2024-04-25 17:54:32 +0300

    fix build

--- routes/stupidity_db.go
+++ routes/stupidity_db.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"server-go/common"
 	"server-go/modules"
 	"strconv"
 )
@@ -18,7 +17,6 @@ var StupidityDBAuth = func(w http.ResponseWriter, r *http.Request) {
 	}
 	http.Redirect(w, r, "receiveToken/"+token, http.StatusTemporaryRedirect)
 }
-
 
 var Admins = func(w http.ResponseWriter, r *http.Request) {
 	admins, err := modules.GetAdmins()
